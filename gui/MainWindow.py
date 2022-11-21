b16dfb369e41af1c14afe8f5309e696ce2303493
Faragó Gyula <faragogyulii@gmail.com>
2022-11-21 21:58:17 +0100

    Basic layout completed, signal wiring in progress.

--- gui/MainWindow.py
+++ gui/MainWindow.py
@@ -15,11 +15,11 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QAbstractItemView, QApplication, QFrame, QHBoxLayout,
-    QHeaderView, QLabel, QLineEdit, QListView,
-    QMainWindow, QMenuBar, QPushButton, QSizePolicy,
-    QStatusBar, QTableWidget, QTableWidgetItem, QVBoxLayout,
-    QWidget)
+from PySide6.QtWidgets import (QAbstractItemView, QApplication, QFrame, QGridLayout,
+    QHBoxLayout, QHeaderView, QLabel, QLineEdit,
+    QListView, QMainWindow, QMenuBar, QPushButton,
+    QSizePolicy, QStatusBar, QTabWidget, QTableView,
+    QTableWidget, QTableWidgetItem, QVBoxLayout, QWidget)
 
 from pyqtgraph import PlotWidget
 
@@ -27,47 +27,107 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(1048, 751)
+        MainWindow.resize(1031, 753)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
-        self.verticalLayout = QVBoxLayout(self.centralwidget)
+        self.horizontalLayout_3 = QHBoxLayout(self.centralwidget)
+        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
+        self.tabWidget = QTabWidget(self.centralwidget)
+        self.tabWidget.setObjectName(u"tabWidget")
+        self.tab = QWidget()
+        self.tab.setObjectName(u"tab")
+        self.verticalLayout = QVBoxLayout(self.tab)
         self.verticalLayout.setObjectName(u"verticalLayout")
-        self.MyLabel = QLabel(self.centralwidget)
+        self.frame_3 = QFrame(self.tab)
+        self.frame_3.setObjectName(u"frame_3")
+        sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(2)
+        sizePolicy.setHeightForWidth(self.frame_3.sizePolicy().hasHeightForWidth())
+        self.frame_3.setSizePolicy(sizePolicy)
+        self.frame_3.setFrameShape(QFrame.StyledPanel)
+        self.frame_3.setFrameShadow(QFrame.Raised)
+        self.verticalLayout_3 = QVBoxLayout(self.frame_3)
+        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
+        self.MyLabel = QLabel(self.frame_3)
         self.MyLabel.setObjectName(u"MyLabel")
+        self.MyLabel.setAlignment(Qt.AlignCenter)
 
-        self.verticalLayout.addWidget(self.MyLabel)
+        self.verticalLayout_3.addWidget(self.MyLabel)
 
-        self.MyGraph = PlotWidget(self.centralwidget)
+        self.frame_4 = QFrame(self.frame_3)
+        self.frame_4.setObjectName(u"frame_4")
+        sizePolicy1 = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(50)
+        sizePolicy1.setHeightForWidth(self.frame_4.sizePolicy().hasHeightForWidth())
+        self.frame_4.setSizePolicy(sizePolicy1)
+        self.frame_4.setFrameShape(QFrame.StyledPanel)
+        self.frame_4.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout_2 = QHBoxLayout(self.frame_4)
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.MyGraph = PlotWidget(self.frame_4)
         self.MyGraph.setObjectName(u"MyGraph")
+        sizePolicy2 = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        sizePolicy2.setHorizontalStretch(10)
+        sizePolicy2.setVerticalStretch(0)
+        sizePolicy2.setHeightForWidth(self.MyGraph.sizePolicy().hasHeightForWidth())
+        self.MyGraph.setSizePolicy(sizePolicy2)
+        self.MyGraph.setSizeIncrement(QSize(0, 0))
 
-        self.verticalLayout.addWidget(self.MyGraph)
+        self.horizontalLayout_2.addWidget(self.MyGraph)
 
-        self.MyEditLine = QLineEdit(self.centralwidget)
+        self.frame_5 = QFrame(self.frame_4)
+        self.frame_5.setObjectName(u"frame_5")
+        sizePolicy3 = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
+        sizePolicy3.setHorizontalStretch(0)
+        sizePolicy3.setVerticalStretch(0)
+        sizePolicy3.setHeightForWidth(self.frame_5.sizePolicy().hasHeightForWidth())
+        self.frame_5.setSizePolicy(sizePolicy3)
+        self.frame_5.setMinimumSize(QSize(75, 192))
+        self.frame_5.setFrameShape(QFrame.Panel)
+        self.frame_5.setFrameShadow(QFrame.Plain)
+        self.frame_5.setLineWidth(1)
+        self.frame_5.setMidLineWidth(2)
+        self.gridLayout_2 = QGridLayout(self.frame_5)
+        self.gridLayout_2.setObjectName(u"gridLayout_2")
+        self.pushButton_2 = QPushButton(self.frame_5)
+        self.pushButton_2.setObjectName(u"pushButton_2")
+
+        self.gridLayout_2.addWidget(self.pushButton_2, 0, 0, 1, 1)
+
+        self.pushButton_3 = QPushButton(self.frame_5)
+        self.pushButton_3.setObjectName(u"pushButton_3")
+
+        self.gridLayout_2.addWidget(self.pushButton_3, 1, 0, 1, 1)
+
+        self.pushButton = QPushButton(self.frame_5)
+        self.pushButton.setObjectName(u"pushButton")
+
+        self.gridLayout_2.addWidget(self.pushButton, 2, 0, 1, 1)
+
+        self.lineEdit = QLineEdit(self.frame_5)
+        self.lineEdit.setObjectName(u"lineEdit")
+
+        self.gridLayout_2.addWidget(self.lineEdit, 3, 0, 1, 1)
+
+
+        self.horizontalLayout_2.addWidget(self.frame_5)
+
+
+        self.verticalLayout_3.addWidget(self.frame_4)
+
+        self.MyEditLine = QLineEdit(self.frame_3)
         self.MyEditLine.setObjectName(u"MyEditLine")
 
-        self.verticalLayout.addWidget(self.MyEditLine)
-
-        self.frame_2 = QFrame(self.centralwidget)
-        self.frame_2.setObjectName(u"frame_2")
-        self.frame_2.setFrameShape(QFrame.StyledPanel)
-        self.frame_2.setFrameShadow(QFrame.Raised)
-        self.horizontalLayout_2 = QHBoxLayout(self.frame_2)
-        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
-        self.MyPushButtonAdd = QPushButton(self.frame_2)
-        self.MyPushButtonAdd.setObjectName(u"MyPushButtonAdd")
-
-        self.horizontalLayout_2.addWidget(self.MyPushButtonAdd)
-
-        self.MyPushButtonDelete = QPushButton(self.frame_2)
-        self.MyPushButtonDelete.setObjectName(u"MyPushButtonDelete")
-
-        self.horizontalLayout_2.addWidget(self.MyPushButtonDelete)
+        self.verticalLayout_3.addWidget(self.MyEditLine)
 
 
-        self.verticalLayout.addWidget(self.frame_2)
+        self.verticalLayout.addWidget(self.frame_3)
 
-        self.frame = QFrame(self.centralwidget)
+        self.frame = QFrame(self.tab)
         self.frame.setObjectName(u"frame")
+        self.frame.setMinimumSize(QSize(0, 60))
         self.frame.setFrameShape(QFrame.StyledPanel)
         self.frame.setFrameShadow(QFrame.Raised)
         self.horizontalLayout = QHBoxLayout(self.frame)
@@ -77,8 +137,47 @@ class Ui_MainWindow(object):
 
         self.horizontalLayout.addWidget(self.MyTable)
 
+        self.frame_2 = QFrame(self.frame)
+        self.frame_2.setObjectName(u"frame_2")
+        self.frame_2.setFrameShape(QFrame.StyledPanel)
+        self.frame_2.setFrameShadow(QFrame.Raised)
+        self.gridLayout = QGridLayout(self.frame_2)
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.StartGraph = QPushButton(self.frame_2)
+        self.StartGraph.setObjectName(u"StartGraph")
+
+        self.gridLayout.addWidget(self.StartGraph, 0, 4, 1, 1)
+
+        self.DebugButton = QPushButton(self.frame_2)
+        self.DebugButton.setObjectName(u"DebugButton")
+
+        self.gridLayout.addWidget(self.DebugButton, 0, 3, 1, 1)
+
+        self.MyPushButtonAdd = QPushButton(self.frame_2)
+        self.MyPushButtonAdd.setObjectName(u"MyPushButtonAdd")
+
+        self.gridLayout.addWidget(self.MyPushButtonAdd, 0, 5, 1, 1)
+
+        self.MyPushButtonDelete = QPushButton(self.frame_2)
+        self.MyPushButtonDelete.setObjectName(u"MyPushButtonDelete")
+
+        self.gridLayout.addWidget(self.MyPushButtonDelete, 1, 5, 1, 1)
+
+        self.PauseGraph = QPushButton(self.frame_2)
+        self.PauseGraph.setObjectName(u"PauseGraph")
+
+        self.gridLayout.addWidget(self.PauseGraph, 1, 4, 1, 1)
+
+
+        self.horizontalLayout.addWidget(self.frame_2)
+
         self.MyList = QListView(self.frame)
         self.MyList.setObjectName(u"MyList")
+        sizePolicy4 = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Minimum)
+        sizePolicy4.setHorizontalStretch(0)
+        sizePolicy4.setVerticalStretch(0)
+        sizePolicy4.setHeightForWidth(self.MyList.sizePolicy().hasHeightForWidth())
+        self.MyList.setSizePolicy(sizePolicy4)
         self.MyList.setMouseTracking(False)
         self.MyList.setEditTriggers(QAbstractItemView.DoubleClicked)
         self.MyList.setSelectionMode(QAbstractItemView.ExtendedSelection)
@@ -89,17 +188,45 @@ class Ui_MainWindow(object):
 
         self.verticalLayout.addWidget(self.frame)
 
+        self.tabWidget.addTab(self.tab, "")
+        self.tab_2 = QWidget()
+        self.tab_2.setObjectName(u"tab_2")
+        self.verticalLayout_4 = QVBoxLayout(self.tab_2)
+        self.verticalLayout_4.setObjectName(u"verticalLayout_4")
+        self.frame_6 = QFrame(self.tab_2)
+        self.frame_6.setObjectName(u"frame_6")
+        self.frame_6.setFrameShape(QFrame.StyledPanel)
+        self.frame_6.setFrameShadow(QFrame.Raised)
+        self.verticalLayout_2 = QVBoxLayout(self.frame_6)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.tableBig = QTableView(self.frame_6)
+        self.tableBig.setObjectName(u"tableBig")
+
+        self.verticalLayout_2.addWidget(self.tableBig)
+
+
+        self.verticalLayout_4.addWidget(self.frame_6)
+
+        self.tabWidget.addTab(self.tab_2, "")
+        self.tab_3 = QWidget()
+        self.tab_3.setObjectName(u"tab_3")
+        self.tabWidget.addTab(self.tab_3, "")
+
+        self.horizontalLayout_3.addWidget(self.tabWidget)
+
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 1048, 22))
+        self.menubar.setGeometry(QRect(0, 0, 1031, 22))
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
         MainWindow.setStatusBar(self.statusbar)
 
         self.retranslateUi(MainWindow)
-        self.MyList.doubleClicked.connect(self.MyList.edit)
+
+        self.tabWidget.setCurrentIndex(0)
+
 
         QMetaObject.connectSlotsByName(MainWindow)
     # setupUi
@@ -107,7 +234,16 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
         self.MyLabel.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
+        self.pushButton_2.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
+        self.pushButton_3.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
+        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
+        self.StartGraph.setText(QCoreApplication.translate("MainWindow", u"StartGraph", None))
+        self.DebugButton.setText(QCoreApplication.translate("MainWindow", u"DebugButton", None))
         self.MyPushButtonAdd.setText(QCoreApplication.translate("MainWindow", u"Add", None))
         self.MyPushButtonDelete.setText(QCoreApplication.translate("MainWindow", u"Delete", None))
+        self.PauseGraph.setText(QCoreApplication.translate("MainWindow", u"PauseGraph", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), QCoreApplication.translate("MainWindow", u"Tab 1", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), QCoreApplication.translate("MainWindow", u"Tab 2", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_3), QCoreApplication.translate("MainWindow", u"Page", None))
     # retranslateUi
 
--- gui/main.py
+++ gui/main.py
@@ -1,39 +1,116 @@
 import json
 import os
 import sys
+import random
+import time
+import uuid
+
+
+from PySide6.QtCore import (
+    QObject,
+    QRunnable,
+    QThreadPool,
+    QTimer,
+    Signal,
+    Slot,
+)
 
 from PySide6.QtCore import QAbstractListModel, Qt
 from PySide6.QtGui import QImage
 from PySide6.QtWidgets import QApplication, QMainWindow
 
+from PySide6.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QPushButton,
+    QVBoxLayout,
+    QWidget,
+)
+
 from MainWindow import Ui_MainWindow
+import pyqtgraph as pg
+
+
+
+
+class GraphSignals(QObject):
+    """
+    Defines the signals available from a running worker thread.
+
+    data
+        tuple data point (worker_id, x, y)
+    """
+
+    data = Signal(tuple)  # <1> Name , value ,time??    
+    pause = Signal(bool)
+
+
+class GraphThread(QRunnable):
+    """
+    Worker thread
+
+    Inherits from QRunnable to handle worker thread setup, signals
+    and wrap-up.
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.worker_id = uuid.uuid4().hex  # Unique ID for this worker.
+        self.signals = GraphSignals()
+        
+         
+
+    @Slot()
+    def run(self):
+        
+        
+        while self.signals.pause:
+            time.sleep(0)  # <1>
+            
+        total_n = 1000
+        y2 = random.randint(0, 10)
+        delay = random.random() / 100  # Random delay value.
+        value = 0
+
+        for n in range(total_n):
+            # Dummy calculation, each worker will produce different values,
+            # because of the random y & y2 values.
+            y = random.randint(0, 10)
+            value += n * y2 - n * y
+
+            self.signals.data.emit((self.worker_id, n, value))  # <2>
+            time.sleep(delay)
+
+
+
+    def pause(self):
+        self.is_paused = True
+
+    def resume(self):
+        self.is_paused = False
+
 
 class TodoModel(QAbstractListModel):
-    def __init__(self, todos=None):
+    def __init__(self, todos_list=None):
         super().__init__()
-        self.todos = todos or []
+        self.todos_list = todos_list or []
 
     def data(self, index, role):
         if role == Qt.DisplayRole:
-            text = self.todos[index.row()]
+            text = self.todos_list[index.row()]
             return text
 
-        if role == Qt.DecorationRole:
-            text = self.todos[index.row()]
-            if text is "done":
-                return "x"
-        #To not be empty during edit
         if role == Qt.EditRole: 
-            text = self.todos[index.row()]
+            text = self.todos_list[index.row()]
             return text
             
     def setData(self, index, value, role):
         if role == Qt.EditRole:
-            self.todos[index.row()] = value
+            self.todos_list[index.row()] = value
             return True
 
     def rowCount(self, index):
-        return len(self.todos)
+        return len(self.todos_list)
     
     def flags(self, index):
             return Qt.ItemIsSelectable|Qt.ItemIsEnabled|Qt.ItemIsEditable
@@ -47,7 +124,55 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.MyList.setModel(self.model)
         self.MyPushButtonAdd.pressed.connect(self.add)
         self.MyPushButtonDelete.pressed.connect(self.delete)
+        self.DebugButton.pressed.connect(self.graph_function)
         
+        self.StartGraph.pressed.connect(self.worker.resume)
+        self.PauseGraph.pressed.connect(self.worker.pause)
+
+        self.threadpool = QThreadPool() # Threading 
+        """
+        Variables for the graphing
+        """
+        self.x = {}  # Keep timepoints.
+        self.y = {}  # Keep data.
+        self.lines = {}  # Keep references to plotted lines, to update.
+
+
+
+    def graph_function(self):
+        worker = GraphThread()
+        worker.signals.data.connect(self.receive_data)
+        # Execute
+        self.threadpool.start(worker)
+        
+    def receive_data(self, data):
+        worker_id, x, y = data  # <3>
+  
+        if worker_id not in self.lines:
+            self.x[worker_id] = [x]
+            self.y[worker_id] = [y]
+
+            # Generate a random color.
+            pen = pg.mkPen(
+                width=2,
+                color=(
+                    random.randint(100, 255),
+                    random.randint(100, 255),
+                    random.randint(100, 255),
+                ),
+            )
+            self.lines[worker_id] = self.MyGraph.plot(
+                self.x[worker_id], self.y[worker_id], pen=pen
+            )
+            
+
+        # Update existing plot/data
+        self.x[worker_id].append(x)
+        self.y[worker_id].append(y)
+
+        self.lines[worker_id].setData(
+            self.x[worker_id], self.y[worker_id]
+        )
 
 
     def add(self):
@@ -55,7 +180,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         input_text = input_text.strip()
         if input_text:  # Don't add empty strings.
             # Access the list via the model.
-            self.model.todos.append(input_text)
+            self.model.todos_list.append(input_text)
             # Trigger refresh.
             self.model.layoutChanged.emit()
             # Empty the input
@@ -65,9 +190,10 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         indexes = self.MyList.selectedIndexes()
         if indexes:
             for index in indexes:
-                del self.model.todos[index.row()]
+                del self.model.todos_list[index.row()]
             self.model.layoutChanged.emit()
             self.MyList.clearSelection()
+
 
     
 
